86f3eb9d8fdc379fbb6cfee0495cd2d65d01ff33
Tamara Organ <tamara@organ.dev.br>
2022-07-06 10:36:24 -0300

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(3333, () => {
+    console.log('ᕦ(ツ)ᕤ Server started at http://localhost:3333');
+});
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express()
+
+app.listen(3333, () => {
+  console.log('ᕦ(ツ)ᕤ Server started at http://localhost:3333')
+})
